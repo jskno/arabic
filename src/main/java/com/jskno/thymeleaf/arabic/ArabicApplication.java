c2b894a46b67f2b36d0cee03d58703a926f80897
jskno <lodmnos@gmail.com>
2018-10-03 08:32:07 +0200

    Arabic

---
+++ src/main/java/com/jskno/thymeleaf/arabic/ArabicApplication.java
@@ -0,0 +1,12 @@
+package com.jskno.thymeleaf.arabic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArabicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArabicApplication.class, args);
+    }
+}
---
+++ src/test/java/com/jskno/thymeleaf/arabic/ArabicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jskno.thymeleaf.arabic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArabicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
